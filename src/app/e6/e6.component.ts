3927960e15d2b1e9cb5c7c40d35b55c2e368f68e
maximemetz <maximemetz@outlook.fr>
2018-05-24 20:14:46 +0200

    fix

--- src/app/e6/e6.component.ts
+++ src/app/e6/e6.component.ts
@@ -7,24 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class E6Component implements OnInit {
 
-  // public isCollapsed1 = false;
-  // public isCollapsed2 = false;
-  // public isCollapsed3 = false;
-  // public isCollapsed4 = false;
-  // public isCollapsed5 = false;
-  // public isCollapsed6 = false;
-  // public isCollapsed7 = false;
-  // public isCollapsed8 = false;
-  // public isCollapsed9 = false;
-  // public isCollapsed10 = false;
-  // public isCollapsed11 = false;
-  // public isCollapsed12 = false;
-  // public isCollapsed13 = false;
-  // public isCollapsed14 = false;
-  // public isCollapsed15 = false;
-  // public isCollapsed16 = false;
-  // public isCollapsed17 = false;
-  // public isCollapsed18 = false;
+  public isCollapsed1 = false;
+  public isCollapsed2 = false;
+  public isCollapsed3 = false;
+  public isCollapsed4 = false;
+  public isCollapsed5 = false;
+  public isCollapsed6 = false;
+  public isCollapsed7 = false;
+  public isCollapsed8 = false;
+  public isCollapsed9 = false;
+  public isCollapsed10 = false;
+  public isCollapsed11 = false;
+  public isCollapsed12 = false;
+  public isCollapsed13 = false;
+  public isCollapsed14 = false;
+  public isCollapsed15 = false;
+  public isCollapsed16 = false;
+  public isCollapsed17 = false;
+  public isCollapsed18 = false;
+  public isCollapsed19 = false;
+  public isCollapsed20 = false;
+  public isCollapsed21 = false;
+  public isCollapsed22 = false;
+
 
   constructor() { }
 
